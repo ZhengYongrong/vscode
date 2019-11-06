2ce23dc112c22eb1a9ba1670f9498351424108c7
zyr <994509352@qq.com>
2019-11-06 10:53:21 +0800

    new

---
+++ VueTest/te.vue
@@ -0,0 +1,25 @@
+<!-- -->
+<template>
+ <div class=''></div>
+</template>
+
+<script>
+export default {
+ name: 'te',
+ data () {
+ return {
+ };
+ },
+
+ components: {},
+
+ filters: {},
+
+ computed: {},
+
+ methods: {}
+}
+
+</script>
+<style lang='less' scoped>
+</style>
---
+++ VueTest/zheng.vue
@@ -0,0 +1,25 @@
+<!-- -->
+<template>
+ <div class=''></div>
+</template>
+
+<script>
+export default {
+ name: 'zheng',
+ data () {
+ return {
+ };
+ },
+
+ components: {},
+
+ filters: {},
+
+ computed: {},
+
+ methods: {}
+}
+
+</script>
+<style lang='less' scoped>
+</style>
